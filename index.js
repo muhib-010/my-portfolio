a84aaa8bfc729c712e7a8965a009c92cd51b2721
muhib-010 <muhibariyan0000@gmail.com>
2025-01-30 15:43:22 +0600

    sadaw

---
+++ index.js
@@ -0,0 +1,52 @@
+document.addEventListener("scroll", () => {
+    const gradientNameDecoy = document.querySelector(".gradient-name-decoy");
+    const gradientName = document.querySelector(".gradient-name");
+    const scrollY = window.scrollY;
+    console.log(scrollY)
+    // Adjust these values based on the design
+    const fadeStart = 100; // Scroll position where sliding starts
+    const fadeEnd = 600; // Scroll position where sliding completes
+    const slideDistance = 500; // Distance to slide to the right
+
+    if (scrollY >= fadeStart && scrollY <= fadeEnd) {
+        const progress = (scrollY - fadeStart) / (fadeEnd - fadeStart);
+        console.log(progress);
+        // Adjust opacity for gradientName
+        gradientName.style.opacity = progress >= 0.2 ? progress : 0;
+        // Change brightness for gradient name
+        gradientNameDecoy.style.filter = `brightness(${1-(progress-0.2)})`
+        // Slide and rotate gradientNameDecoy
+        gradientNameDecoy.style.transform = `translate(calc(-50% + ${progress * slideDistance}px), -50%)`;
+        // Set display to initial as long as progress is not 100%
+        gradientNameDecoy.style.display = "initial";
+    } else if (scrollY >= fadeEnd) {
+        // Completely slid out, set display to none
+        gradientNameDecoy.style.transform = `translate(calc(-50% + ${slideDistance}px), 50%)`;
+        gradientNameDecoy.style.display = "none"; // Hide the element completely
+    } else {
+        // Reset to initial state and make it visible
+        gradientNameDecoy.style.transform = "translate(-50%, -50%) rotateY(0deg)";
+        gradientNameDecoy.style.opacity = "initial"; // Make sure it's visible again
+    }
+});
+
+
+
+
+
+
+
+function navExtend() {
+    console.log("pressed");
+    
+    const navContainer = document.querySelector('.nav-elem-container'); // Get the navbar container
+    const navLinks = document.querySelectorAll(".nav-elem"); // Get all nav elements
+    const gradientName = document.querySelector('.gradient-name');
+    // Toggle the 'show' class on each nav element
+    navLinks.forEach(link => {
+        link.classList.toggle("show");
+    });
+    // Toggle the 'nav-elem-container-extend' class on the container
+    navContainer.classList.toggle('nav-elem-container-extend');
+    gradientName.classList.toggle('hide');
+}
