0031af886a3ca28f5d0f727f4cb3118d8123a4b4
muhib-010 <muhibariyan0000@gmail.com>
2025-03-18 19:02:56 +0600

    Update vite.config.js

--- projects/crazy-math-game/vite.config.js
+++ projects/crazy-math-game/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/my-portfolio/projects/crazy-math-game/",
+  base: "./my-portfolio/projects/crazy-math-game/",
   plugins: [tailwindcss(), react()],
 });
