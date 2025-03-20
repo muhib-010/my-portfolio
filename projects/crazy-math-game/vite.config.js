0599dce7d926b7f388724b39f7194b96b191b07b
muhib-010 <muhibariyan0000@gmail.com>
2025-03-20 17:33:14 +0600

    idk

--- projects/crazy-math-game/vite.config.js
+++ projects/crazy-math-game/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: "/", // Ensures relative paths work in the build
+  base: "./", // Ensures relative paths work in the build
   plugins: [react(), tailwindcss()],
 });
 
