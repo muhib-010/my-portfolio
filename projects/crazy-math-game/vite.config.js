d9253df2ca81155df266f497909b01a0b9f70330
muhib-010 <muhibariyan0000@gmail.com>
2025-03-22 18:04:04 +0600

    i hope its fixed now

--- projects/crazy-math-game/vite.config.js
+++ projects/crazy-math-game/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: "/my-portfolio/projects/crazy-math-game/",
+  base: "/my-portfolio/projects/crazy-math-game",
   plugins: [react(), tailwindcss()],
 });
 
